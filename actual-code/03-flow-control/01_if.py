f241bd28f34495440c76f915d2246aac69265f8d
Kevin Cunningham <kevin@kevincunningham.co.uk>
2026-04-27 12:31:29 +0100

    Adds start of flow c ontrol

---
+++ actual-code/03-flow-control/01_if.py
@@ -0,0 +1,10 @@
+number_input = int(input("Give me a number: "))
+
+# !=  <   >  <=  >=
+if number_input == 42:  # about to enter a block of code
+  print("That is the right number, well done!")
+  print("Have a good day")
+else:
+  print("That is the wrong answer.")
+
+print("End of program.")
